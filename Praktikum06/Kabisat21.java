bbbacbd655219ff8d3d95febad231fed602e9b0b
raysyid <ridhxrasyid@gmail.com>
2023-10-11 11:49:45 +0700

    aturan tambahan

--- Praktikum06/Kabisat21.java
+++ Praktikum06/Kabisat21.java
@@ -10,10 +10,12 @@ public class Kabisat21 {
         System.out.print("Masukkan tahun: ");
         tahun = sc.nextInt();
 
-        if ((tahun % 4) == 0 && (tahun % 100) != 0) {
+        if ((tahun % 4) == 0 && (tahun % 100) != 0 || (tahun % 400) == 0 ){
             System.out.println("Tahun Kabisat");
-        } else
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+        }
+            
     }
 
     
