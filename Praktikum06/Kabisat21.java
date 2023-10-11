c267dfbe1c7dbbe16d918922a98b43e36a65e1d9
raysyid <ridhxrasyid@gmail.com>
2023-10-11 11:15:39 +0700

    kabisat operator logika

---
+++ Praktikum06/Kabisat21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kabisat21 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = sc.nextInt();
+
+        if ((tahun % 4) == 0 && (tahun % 100) != 0) {
+            System.out.println("Tahun Kabisat");
+        } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+
+    
+}
