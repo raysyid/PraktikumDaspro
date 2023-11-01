07b0de71764e9594c96b02c23a14cec595f1f980
raysyid <ridhxrasyid@gmail.com>
2023-11-01 14:57:10 +0700

    tidak ditemukan

--- Praktikum09/LinearSearch21.java
+++ Praktikum09/LinearSearch21.java
@@ -1,17 +1,37 @@
+import java.util.Scanner;
+
 public class LinearSearch21 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         
-        int[] arrayint = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        int arrayint[], key, hasil = -1, elemen;
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        elemen = sc.nextInt();
+        arrayint = new int[elemen];
+        System.out.println("");
+
+        for (int i = 0; i < arrayint.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + ": ");
+            arrayint[i] = sc.nextInt();
+        }
+        System.out.println("");
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+        System.out.println("");
 
         for (int i = 0; i < arrayint.length; i++) {
             if (arrayint[i] == key) {
                 hasil = i;
+                System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+
+        if (hasil == -1) {
+            System.out.println("Key tidak ditemukan");
+        }
     }
-    
 }
