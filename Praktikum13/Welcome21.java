5d37cd28f292ee59bb94d67b457f3570984d0ee2
raysyid <ridhxrasyid@gmail.com>
2023-11-29 10:48:49 +0700

    display message

---
+++ Praktikum13/Welcome21.java
@@ -0,0 +1,9 @@
+public class Welcome21 {
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+
+    public static void displayWelcomeMessage() {
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+    }
+}
