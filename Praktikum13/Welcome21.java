28df957b735e07aa14c65fe31c2cf7374e12d633
raysyid <ridhxrasyid@gmail.com>
2023-11-29 11:23:50 +0700

    display name parameter

--- Praktikum13/Welcome21.java
+++ Praktikum13/Welcome21.java
@@ -1,9 +1,5 @@
 public class Welcome21 {
     public static void main(String[] args) {
-        displayWelcomeMessage();
-    }
-
-    public static void displayWelcomeMessage() {
         System.out.println("Selamat datang di Politeknik Negeri Malang");
     }
 }
---
+++ Praktikum13/Welcome221.java
@@ -0,0 +1,12 @@
+public class Welcome221 {
+    public static void main(String[] args) {
+        displayWelcomeMessage("Ani");
+        displayWelcomeMessage("Budi");
+        displayWelcomeMessage("Cica");
+        displayWelcomeMessage("Dodi");
+    }
+
+    public static void displayWelcomeMessage(String name) {
+        System.out.println("Selamat datang di Politeknik Negeri Malang, " + name);
+    }
+}
