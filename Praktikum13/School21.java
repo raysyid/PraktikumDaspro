4187501faf6614e598f689e6113db63cac5d8f8a
raysyid <ridhxrasyid@gmail.com>
2023-11-29 13:23:09 +0700

    School+

---
+++ Praktikum13/School21.java
@@ -0,0 +1,25 @@
+public class School21 {
+    public static void main(String[] args) {
+        String[] students1A = { "Anne", "Aldrian", "Ali", "Arul", "Ashley" };
+        String[] students1B = { "Bobby", "Brian", "Bill" };
+        String[] students1C = { "Clara", "Celine", "Charlie", "Caroline" };
+        String[] students1D = { "Daniel", "Daisy", "Diana" };
+
+        display(students1A);
+        display(students1B);
+        display(students1C);
+        display(students1D);    
+    }
+
+    public static void display(String[] students) {
+        System.out.printf("+++++++++++++++++++++++++%n");
+        System.out.printf("| %-3s | %-15s |%n", "NO", "NAME");
+        System.out.printf("+++++++++++++++++++++++++%n");
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.printf("| %-3d | %-15s |%n", (i + 1), students[i]);
+        }
+
+        System.out.printf("+++++++++++++++++++++++++%n%n");
+    }
+}
